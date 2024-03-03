17e97dae8df1fc059b3a6104549a23ef14d7a43b
rehanmosabbir <rehanmosabbir@gmail.com>
2024-03-03 09:07:28 +0600

    add add to cart functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import MovieList from "./components/MovieList";
 import Sidebar from "./components/Sidebar";
+import { MovieContext } from "./context";
 
 function App() {
+  const [cartData, setCartData] = useState([]);
   return (
-    <>
+    <MovieContext.Provider value={{ cartData, setCartData }}>
       <Header />
       <main>
         <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
@@ -12,7 +16,8 @@ function App() {
           <MovieList />
         </div>
       </main>
-    </>
+      <Footer />
+    </MovieContext.Provider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,65 @@
+import { useContext, useState } from "react";
 import Moon from "../assets/icons/moon.svg";
 import Logo from "../assets/logo.svg";
 import Ring from "../assets/ring.svg";
 import ShoppingCart from "../assets/shopping-cart.svg";
+import { MovieContext } from "../context";
+import CartDetails from "./CartDetails";
 
 export default function Header() {
-  return (
-    <header>
-      <nav className="container flex items-center justify-between space-x-10 py-6">
-        <a href="index.html">
-          <img src={Logo} width="139" height="26" alt="Logo" />
-        </a>
+  const [showCart, setShowCart] = useState(false);
 
-        <ul className="flex items-center space-x-5">
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href=""
-            >
-              <img src={Ring} width="24" height="24" alt="Ring" />
-            </a>
-          </li>
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href=""
-            >
-              <img src={Moon} width="24" height="24" alt="Moon" />
-            </a>
-          </li>
-          <li>
-            <a
-              className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
-              href="#"
-            >
-              <img src={ShoppingCart} width="24" height="24" alt="Cart" />
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+  const { cartData } = useContext(MovieContext);
+
+  function handleCartDetails() {
+    setShowCart(true);
+  }
+  function handleCloseCartModal() {
+    setShowCart(false);
+  }
+  return (
+    <>
+      {showCart && <CartDetails onClose={handleCloseCartModal} />}
+      <header>
+        <nav className="container flex items-center justify-between space-x-10 py-6">
+          <a href="index.html">
+            <img src={Logo} width="139" height="26" alt="Logo" />
+          </a>
+
+          <ul className="flex items-center space-x-5">
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href=""
+              >
+                <img src={Ring} width="24" height="24" alt="Ring" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href=""
+              >
+                <img src={Moon} width="24" height="24" alt="Moon" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+                onClick={handleCartDetails}
+              >
+                <img src={ShoppingCart} width="24" height="24" alt="Cart" />
+                {cartData.length > 0 && (
+                  <span className=" rounded-full absolute top-[-12px] left-[20px] bg-[#12cf6f] text-white text-center p-[2px] w-[30px] h-[30px]">
+                    {cartData.length}
+                  </span>
+                )}
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
